be6d4535817990db41b8195dca6121e387718d78
AbdulMateen-X <mateen0bhat@gmail.com>
2026-01-21 17:42:53 +0530

    .

--- Basic Recursion/_01_Sum_Of_N_Natural_Numbers.java
+++
@@ -1,7 +0,0 @@
-public class _01_Sum_Of_N_Natural_Numbers{
-    public int findSum(int n){
-        if(n==1)
-            return 1;
-        return n + findSum(n-1);
-    }
-}
--- Basic Recursion/_02_Factorial.java
+++
@@ -1,8 +0,0 @@
-public class _02_Factorial {
-    public int fact(int n){
-        if(n==1 || n==0)
-            return 1;
-        return n*fact(n-1);
-
-    }
-}
